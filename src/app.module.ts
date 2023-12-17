09d15d864a0f8a482d1906e6d4a1e716b661b426
Sayantan <sayantann2002@gmail.com>
2023-12-17 13:02:05 +0530

    Feat : JWT validatiom created ✅

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { ArticlesModule } from './articles/articles.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, ArticlesModule, UsersModule],
+  imports: [
+    PrismaModule,
+    ArticlesModule,
+    UsersModule,
+    AuthModule,
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+  ],
   controllers: [],
   providers: [],
 })
